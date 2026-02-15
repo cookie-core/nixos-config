9525252a7aee2943a6eddf79070e269252993109
cookie <nezeret09x2@gmail.com>
2026-02-15 20:55:13 +0300

    idk

--- flake.nix
+++ flake.nix
@@ -1,32 +1,14 @@
 {
-  description = "NixOS configuration with zapret-discord-youtube";
+  description = "super cookie NixOS config!";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    zapret-discord-youtube.url = "github:kartavkun/zapret-discord-youtube";
-    minegrub-theme.url = "github:Lxtharia/minegrub-theme";
   };
 
   outputs = { self, nixpkgs, ... } @ inputs: {
-    nixosConfigurations.cookie = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-
-        inputs.zapret-discord-youtube.nixosModules.default
-        {
-          services.zapret-discord-youtube = {
-            enable = true;
-            config = "general(ALT10)";
-            listGeneral = [ "youtube.com" "youtu.be" ];
-            listExclude = [ "chatgpt.com" ];
-            ipsetAll = [ "192.168.1.0/24" "10.0.0.1" ];
-            ipsetExclude = [ "203.0.113.0/24" ];
-          };
-        }
-        
-        # inputs.minegrub-theme.nixosModules.default
-      ];
-    };
+    system = "x86_64-linux";
+    modules = [
+      ./configuration.nix
+    ];
   };
 }
