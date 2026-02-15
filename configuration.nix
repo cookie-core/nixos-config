a2b97db63cce0d03ceb6e3a237cc251833755a8e
cookie <nezeret09x2@gmail.com>
2026-02-15 22:03:31 +0300

    asldas

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.efiSupport = true;
+  boot.loader.grub.theme = ./cookie-grub-theme;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
   
