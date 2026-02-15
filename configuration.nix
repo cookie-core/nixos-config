be0ce30385fe09c9a393b848c417301dd7f8fbe5
cookie <nezeret09x2@gmail.com>
2026-02-15 20:44:04 +0300

    my old config

---
+++ configuration.nix
@@ -0,0 +1,155 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+
+  boot.loader.grub.theme = pkgs.sleek-grub-theme;
+  # boot.loader.grub.theme = (pkgs.sleek-grub-theme.override {
+  #    withBanner = "idk";
+  #    withStyle = "dark";
+  # });
+  boot.loader.grub.splashImage = ./frieren_static2.png;
+  boot.loader.grub.gfxmodeEfi = "1920x1080";
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ "i915.modeset=1" ];
+  
+  # boot.loader.grub.minegrub-theme = {
+  #    enable = true;
+  #    splash = "yooo!";
+  #    background = "background_options/1.8  - [Classic Minecraft].png";
+  #    boot-options-count = 5;
+  # };
+
+  networking.hostName = "nixos";
+
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+
+  time.timeZone = "Europe/Moscow";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [
+    "ru_RU.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+  ];
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  programs.hyprland.enable = true;
+
+  hardware.opengl.enable = true;
+  hardware.opengl.extraPackages = with pkgs; [
+    intel-vaapi-driver
+    libva
+  ];
+
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  services.xserver.libinput.enable = true;
+
+  users.users.cookie = {
+    isNormalUser = true;
+    description = "Cookie";
+    extraGroups = [ "networkmanager" "wheel" ];
+    useDefaultShell = true;
+  };
+  users.defaultUserShell = pkgs.zsh;
+
+  programs.firefox.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      # theme = "powerlevel10k/powerlevel10k";
+    };
+    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+  };
+
+  environment.systemPackages = with pkgs; [
+    hyprland
+    hyprlock
+    brightnessctl
+    networkmanager
+    bluez
+    stress-ng
+    mpvpaper
+    foot
+    mako
+    wofi
+    wofi-emoji
+    waybar
+    neovim
+    neovide
+    python3
+    gcc
+    pkgs.llvmPackages_20.clang-unwrapped
+    lua
+    lua51Packages.lua
+    luaPackages.luarocks
+    kitty
+    unzip
+    wget
+    git
+    kdePackages.dolphin
+    telegram-desktop
+    discord
+    fastfetch
+    nwg-look
+    grim
+    grimblast
+    slurp
+    wl-clipboard
+  ];
+  fonts.packages = with pkgs; [
+    noto-fonts-color-emoji
+    nerd-fonts.jetbrains-mono
+    roboto
+    roboto-serif
+  ];
+
+  fonts.fontconfig.defaultFonts = {
+    serif = [ "Roboto Serif" ];
+    sansSerif = [ "Roboto" ];
+    monospace = [ "JetBrainsMono Nerd Font Mono" ];
+    emoji = [ "NoTo Color Emoji" ];
+  };
+
+  system.stateVersion = "25.11";
+}
--- flake.nix
+++ flake.nix
@@ -1,15 +1,32 @@
 {
-  description = "A very basic flake";
+  description = "NixOS configuration with zapret-discord-youtube";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zapret-discord-youtube.url = "github:kartavkun/zapret-discord-youtube";
+    minegrub-theme.url = "github:Lxtharia/minegrub-theme";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... } @ inputs: {
+    nixosConfigurations.cookie = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+        inputs.zapret-discord-youtube.nixosModules.default
+        {
+          services.zapret-discord-youtube = {
+            enable = true;
+            config = "general(ALT10)";
+            listGeneral = [ "youtube.com" "youtu.be" ];
+            listExclude = [ "chatgpt.com" ];
+            ipsetAll = [ "192.168.1.0/24" "10.0.0.1" ];
+            ipsetExclude = [ "203.0.113.0/24" ];
+          };
+        }
+        
+        # inputs.minegrub-theme.nixosModules.default
+      ];
+    };
   };
 }
