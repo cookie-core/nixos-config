0bf11ec0676e96d7d1d07d65dc85f749397eb532
cookie <nezeret09x2@gmail.com>
2026-02-21 16:13:53 +0300

    doing grub theme

---
+++ cookie-grub-theme/codes/create_images.py
@@ -0,0 +1,59 @@
+from PIL import Image
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parent
+menu_background_path = base_dir / "../menu-background.png"
+item_style_folder = base_dir / "../item-styled-box/"
+selected_item_style_folder = base_dir / "../selected-item-styled-box/"
+
+item_line_size = 5
+base_size = 100
+background_color = "#1C1C1C9C"
+item_color = "#EDFFFFFF"
+selected_item_color = "#7A9EDBFF"
+
+menu_background = Image.new("RGBA", ( base_size, base_size ), background_color)
+menu_background.save(menu_background_path)
+
+corners = [ "nw", "ne", "se", "sw" ]
+corners_size = ( item_line_size, item_line_size )
+
+for i in range(4):
+    current_item_color = "#00000000"
+    current_selected_item_color = "#00000000"
+
+    if i == 0 or i == 3:
+        current_item_color = item_color
+        current_selected_item_color = selected_item_color
+
+    item = Image.new("RGBA", corners_size, current_item_color)
+    selected_item = Image.new("RGBA", corners_size, current_selected_item_color)
+    
+    item.save(f"{item_style_folder}/{corners[i]}.png")
+    selected_item.save(f"{selected_item_style_folder}/{corners[i]}.png")
+
+
+borders = [ "w", "n", "e", "s" ]
+borders_sizes = [ ( item_line_size, base_size ), ( base_size, item_line_size ) ]
+
+for i in range(4):
+    current_item_color = "#00000000"
+    current_selected_item_color = "#00000000"
+
+    if i == 0:
+        current_item_color = item_color
+        current_selected_item_color = selected_item_color
+
+    item = Image.new("RGBA", borders_sizes[i % 2], current_item_color)
+    selected_item = Image.new("RGBA", borders_sizes[i % 2], current_selected_item_color)
+
+    item.save(f"{item_style_folder}/{borders[i]}.png")
+    selected_item.save(f"{selected_item_style_folder}/{borders[i]}.png")
+
+
+item_center = Image.new("RGBA", ( base_size, base_size ), "#00000000")
+selected_item_center = Image.new("RGBA", ( base_size, base_size ), "#00000000")
+item_center.save(f"{item_style_folder}/c.png")
+selected_item_center.save(f"{selected_item_style_folder}/c.png")
+
+
