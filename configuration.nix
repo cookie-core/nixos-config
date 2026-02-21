2fa7a4923b81ff9a7a7bf78552ece496e5876846
cookie <nezeret09x2@gmail.com>
2026-02-21 22:22:34 +0300

    doing grub theme

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.efiSupport = true;
+  boot.loader.grub.gfxmodeEfi = "auto";
   # boot.loader.grub.splashImage = ./cookie-grub-theme/background.png;
   # boot.loader.grub.splashMode = "normal";
   boot.loader.grub.theme = ./cookie-grub-theme;
